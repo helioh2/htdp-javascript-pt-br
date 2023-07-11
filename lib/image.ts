8470cfae9202e801e5b6187eee7c79ff25cce98c
Helio <heliohenrique3@gmail.com>
2023-07-11 18:08:01 -0300

    Carregando imagem de arquivo.

--- lib/image.ts
+++ lib/image.ts
@@ -3,45 +3,14 @@
 const canvas = document.getElementById("tela") as HTMLCanvasElement | null;
 
 export enum ModoImagem {
-    OUTLINE,
-    SOLID
+    OUTLINE="outline",
+    SOLID="solid"
 }
 
 export abstract class Imagem {
     
     abstract desenha(x?: number|null, y?: number|null): void;
 
-}
-
-class Retangulo extends Imagem {
-
-    largura: number;
-    altura: number;
-    cor: string;
-    modo: ModoImagem;
-
-    constructor(largura: number, altura: number, cor: string, modo: ModoImagem) {
-        super()
-        this.largura = largura;
-        this.altura = altura;
-        this.cor = cor;
-        this.modo = modo;
-    }
-
-    desenha(x: number|null = null, y: number|null = null) {
-
-        x = x == null? canvas!.width/2: x;
-        y = y == null? canvas!.height/2: y;
-
-        let ctx = canvas!.getContext("2d");
-        if (this.modo == ModoImagem.OUTLINE) {
-            ctx!.strokeStyle = this.cor;
-            ctx!.strokeRect(x, y, this.largura, this.altura);
-        } else {
-            ctx!.fillStyle = this.cor;
-            ctx!.fillRect(x, y, this.largura, this.altura);
-        }
-    }
 }
 
 
@@ -87,6 +56,96 @@ class ImagemComposta extends Imagem {
 
 }
 
+abstract class RetLike extends Imagem {
+
+    largura: number;
+    altura: number;
+    cor: string;
+    modo: ModoImagem;
+
+    constructor(largura: number, altura: number, cor: string, modo: ModoImagem) {
+        super()
+        this.largura = largura;
+        this.altura = altura;
+        this.cor = cor;
+        this.modo = modo;
+    }
+}
+
+class Retangulo extends RetLike {
+ 
+    desenha(x: number|null = null, y: number|null = null) {
+
+        x = x == null? canvas!.width/2: x;
+        y = y == null? canvas!.height/2: y;
+
+        let ctx = canvas!.getContext("2d");
+        if (this.modo == ModoImagem.OUTLINE) {
+            ctx!.strokeStyle = this.cor;
+            ctx!.strokeRect(x-this.largura/2, y-this.altura/2, this.largura, this.altura);
+        } else {
+            ctx!.fillStyle = this.cor;
+            ctx!.fillRect(x-this.largura/2, y-this.altura/2, this.largura, this.altura);
+        }
+    }
+}
+
+
+class Elipse extends RetLike {
+
+    desenha(x: number|null = null, y: number|null = null) {
+
+        x = x == null? canvas!.width/2: x;
+        y = y == null? canvas!.height/2: y;
+
+        let ctx = canvas!.getContext("2d");
+        ctx!.beginPath();
+        ctx!.ellipse(x, y, this.largura/2, this.altura/2, 0, 0, 2*Math.PI);
+        if (this.modo == ModoImagem.OUTLINE) {
+            ctx!.stroke();
+            ctx!.strokeStyle = this.cor;
+        } else {
+            ctx!.fill();
+            ctx!.fillStyle = this.cor;
+        }      
+    }
+}
+
+
+class ImagemDeArquivo extends Imagem {
+    src: string;
+    img: HTMLImageElement;
+    largura: number;
+    altura: number;
+
+    constructor(src: string, largura: number, altura: number) {
+        super();
+        this.largura = largura;
+        this.altura = altura;
+        this.src = src;
+        this.img = new Image();
+        this.img.src = this.src
+    }
+
+    desenha(x: number|null = null, y: number|null = null) {
+        x = x == null? canvas!.width/2: x;
+        y = y == null? canvas!.height/2: y;
+
+        let ctx = canvas!.getContext("2d");
+        if (this.img.complete) {           
+            ctx!.drawImage(this.img, x!-this.largura/2, y!-this.altura/2, this.largura, this.altura);
+        } else {
+            this.img.onload = () => {
+                ctx!.drawImage(this.img, x!-this.largura/2, y!-this.altura/2, this.largura, this.altura);
+            };
+        }
+
+    }
+}
+
+
+
+
 function limparCanvas() {
     let ctx = canvas!.getContext("2d");
     ctx!.clearRect(0, 0, canvas!.width, canvas!.height);
@@ -96,14 +155,30 @@ function limparCanvas() {
 
 // FUNÇÕES EXPORTADAS:
 
-export function retangulo(largura: number, altura: number, cor: string, modo: ModoImagem): Retangulo {
-    return new Retangulo(largura, altura, cor, modo);
-}
-
 export function colocarImagem(imagemFrente: Imagem, x: number, y: number, imagemFundo: ImagemVazia|ImagemComposta) {
     return new ImagemComposta(imagemFrente, x, y, imagemFundo)
 }
 
 export function cenaVazia(largura: number, altura: number) {
     return new ImagemVazia(largura, altura);
+}
+
+export function retangulo(largura: number, altura: number, cor: string = "black", modo: string = "outline"): Retangulo {
+    return new Retangulo(largura, altura, cor, modo as ModoImagem);
+}
+
+export function elipse(largura: number, altura: number, cor: string = "black", modo: string = "outline"): Elipse {
+    return new Elipse(largura, altura, cor, modo as ModoImagem);
+}
+
+export function quadrado(lado: number, cor: string = "black", modo: string = "outline"): Retangulo {
+    return new Retangulo(lado, lado, cor, modo as ModoImagem);
+}
+
+export function circulo(raio: number, cor: string = "black", modo: string = "outline"): Elipse {
+    return new Elipse(raio*2, raio*2, cor, modo as ModoImagem);
+}
+
+export function carregarImagem(nomeArquivo: string, largura: number = 50, altura: number = 50) {
+    return new ImagemDeArquivo(nomeArquivo, largura, altura);
 }
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,24 @@
-import { Imagem, ModoImagem, cenaVazia, colocarImagem, retangulo } from "../lib/image"
+import { Imagem, ModoImagem, carregarImagem, cenaVazia, circulo, colocarImagem, elipse, quadrado, retangulo } from "../lib/image"
 import { bigBang } from "../lib/universe";
 
 
 const ret1 = retangulo(50, 50, "blue", ModoImagem.OUTLINE);
 const ret2 = retangulo(150, 50, "blue", ModoImagem.SOLID);
 
-const LARGURA = 800;
-const ALTURA = 500;
+const LARGURA = 300;
+const ALTURA = 300;
 
 const TELA_VAZIA = cenaVazia(LARGURA, ALTURA);
 
 const IMAGEM = colocarImagem(ret2, 100, 100, colocarImagem(ret1, 20, 40, TELA_VAZIA));
 
-const FOGUETE = retangulo(50, 50, "red", ModoImagem.SOLID);
+const FOGUETE = carregarImagem("./foguete.png");
+
+// const FOGUETE = retangulo(50, 50, "blue", ModoImagem.OUTLINE);
+
+FOGUETE.desenha()
+// FOGUETE2.desenha()
+
 
 const X_FOGUETE = LARGURA / 2
 
@@ -50,16 +56,21 @@ function trataMouse(foguete: number, x: number, y: number, evType) {
 
 
 function pararQuando(foguete: number) {
-    return foguete >= ALTURA-50
+    return foguete >= ALTURA-50/2
 }
 
-bigBang(0, 
-    {
-        aCadaTick: desce, 
-        desenhar: desenhar, 
-        frequencia: 60,
-        quandoTecla: trataTecla,
-        quandoMouse: trataMouse,
-        pararQuando: pararQuando
-    });
+function main() {
+    bigBang(0, 
+        {
+            aCadaTick: desce, 
+            desenhar: desenhar, 
+            frequencia: 60,
+            quandoTecla: trataTecla,
+            quandoMouse: trataMouse,
+            pararQuando: pararQuando
+        });
+}
+
+
+main();
 
