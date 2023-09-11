00d6e6281d62293b1493d83e5b465acb024d7a92
Helio <heliohenrique3@gmail.com>
2023-09-11 15:51:45 -0300

    Estruturas para chupacabra e jogo.

--- src/exemplos/vaca/vaca_inic0.ts
+++ src/exemplos/vaca/vaca_inic0.ts
@@ -1,8 +1,7 @@
-import { Imagem, alturaImagem, carregarImagem, cenaVazia, colocarImagem, espelhar, larguraImagem, redimensionar } from "../../../lib/image"
+import { Imagem, carregarImagem, cenaVazia, colocarImagem, espelhar, larguraImagem, redimensionar } from "../../../lib/image"
 import { reactor } from "../../../lib/universe";
 import { testes } from "../../../lib/utils";
 import imgVacaInoUrl from "./vaca-ino.png";
-import imgCcEsqUrl from "./chupacabra.png";
 // CONSTANTES:
 
 
@@ -10,23 +9,15 @@ const [LARGURA, ALTURA] = [600, 400]
 
 const TELA = cenaVazia(LARGURA, ALTURA)
  
-const IMG_VACA_INO = carregarImagem(imgVacaInoUrl, 100, 60);
+const IMG_VACA_INO = carregarImagem(imgVacaInoUrl, 100, 70);
 const IMG_VACA_VORTANO = espelhar(IMG_VACA_INO);
-
-const IMG_CC_ESQ = carregarImagem(imgCcEsqUrl, 60, 100);
-const IMG_CC_DIR = espelhar(IMG_CC_ESQ);
 
 const Y_INICIAL_VACA = ALTURA / 2
 
 const LIMITE_ESQUERDA_VACA = 0 + larguraImagem(IMG_VACA_INO) / 2
 const LIMITE_DIREITA_VACA = LARGURA - larguraImagem(IMG_VACA_INO) / 2
-const LIMITE_BAIXO_VACA = ALTURA - alturaImagem(IMG_VACA_INO) / 2
-const LIMITE_CIMA_VACA = 0 + alturaImagem(IMG_VACA_INO) / 2
-
-const LIMITE_ESQUERDA_CC = 0 + larguraImagem(IMG_CC_ESQ) / 2
-const LIMITE_DIREITA_CC = LARGURA - larguraImagem(IMG_CC_ESQ) / 2
-const LIMITE_BAIXO_CC = ALTURA - alturaImagem(IMG_CC_ESQ) / 2
-const LIMITE_CIMA_CC = 0 + alturaImagem(IMG_CC_ESQ) / 2
+const LIMITE_BAIXO_VACA = ALTURA - larguraImagem(IMG_VACA_INO) / 2
+const LIMITE_CIMA_VACA = 0 + larguraImagem(IMG_VACA_INO) / 2
 
 const DX_PADRAO = 3
 
@@ -39,6 +30,7 @@ interface Vaca {
     dx: number,
     dy: number
 }
+
 function makeVaca(x: number, y: number, dx: number, dy: number): Vaca {
     return { x: x, y: y, dx: dx, dy: dy };
 }
@@ -47,76 +39,24 @@ function makeVaca(x: number, y: number, dx: number, dy: number): Vaca {
  * interp. representa a posicao x e y da vaca, e o deslocamento
  * a cada tick no eixo x e y, chamados de dx e dy
  */
+
+
 // EXEMPLOS:
-const VACA_SEM_MAKE: Vaca = {
-    x: 200,
-    y: 300,
-    dx: 3,
-    dy: 0
-}
 const VACA_INICIAL = makeVaca(LIMITE_ESQUERDA_VACA, Y_INICIAL_VACA, DX_PADRAO, 0)
-const VACA_INICIAL_PROX = makeVaca(LIMITE_ESQUERDA_VACA + DX_PADRAO, Y_INICIAL_VACA, DX_PADRAO, 0)
+const VACA_INICIAL2 = makeVaca(LIMITE_ESQUERDA_VACA + DX_PADRAO, Y_INICIAL_VACA, DX_PADRAO, 0)
 const VACA0 = makeVaca(LIMITE_ESQUERDA_VACA, Y_INICIAL_VACA, 3, 4)
 const VACA1 = makeVaca(LIMITE_ESQUERDA_VACA + 3, Y_INICIAL_VACA + 4, 3, 4)
-const VACA_MEIO = makeVaca(LARGURA/2, ALTURA/2, 3, 0)
 // const VACA_MEIO = (x: LARGURA/2, y: 0, dx: 3, dy:0}
 const VACA_FIM = makeVaca(LIMITE_DIREITA_VACA + 1, LIMITE_BAIXO_VACA, 3, 0)
 const VACA_VIRANDO = makeVaca(LIMITE_DIREITA_VACA, LIMITE_BAIXO_VACA, -3, 0)
 const VACA_VORTANO = makeVaca(LARGURA / 2, LIMITE_BAIXO_VACA, -3, 0)
 
-// ----------------
-
-interface Chupacabra {
-    x: number,
-    y: number,
-    dy: number
-}
-function makeChupacabra(x: number, y: number, dy: number): Chupacabra {
-    return { x: x, y: y, dy: dy };
-}
-/**
- * Como criar objeto: makeChupacabra(Int, Int, Int) ou {x: Int, y: Int, dy: Int}
- * interp. representa a posicao x e y de um cc, e o deslocamento
- * a cada tick no eixo y, chamado de dy
- */
-//EXEMPLOS:
-const CC_INICIAL = makeChupacabra(LARGURA/2, LIMITE_CIMA_CC, 2);
-const CC_INICIAL_PROX = makeChupacabra(LARGURA/2, LIMITE_CIMA_CC+2, 2);
-const CC_INICIAL_PROX_PROX = makeChupacabra(LARGURA/2, LIMITE_CIMA_CC+2+2, 2);
-const CC_MEIO = makeChupacabra(LARGURA/2, ALTURA/2, 2);
-const CC_BAIXO = makeChupacabra(LARGURA/2, LIMITE_BAIXO_CC+1, 2);
-const CC_BAIXO_VIRANDO = makeChupacabra(LARGURA/2, LIMITE_BAIXO_CC, -2);
-const CC_BAIXO_VIRANDO_PROX = makeChupacabra(LARGURA/2, LIMITE_BAIXO_CC-2, -2);
-const CC_CIMA = makeChupacabra(LARGURA/2, LIMITE_CIMA_CC-1, -2);
-const CC_CIMA_VIRANDO = CC_INICIAL
-
-// ------------
-
-interface Jogo {
-    vaca: Vaca, 
-    cc: Chupacabra,
-    gameOver: boolean
-}
-function makeJogo(vaca: Vaca, cc: Chupacabra, gameOver: boolean) {
-    return {vaca: vaca, cc: cc, gameOver: gameOver};
-}
-/**
- * Como criar objeto: makeJogo(Vaca, Chupacabra, boolean) ou {vaca: Vaca, cc: Chupacabra, gameOver: boolean}
- * interp. representa o jogo com todos os seus elementos,
- * incluindo a vaca (personagem controlado), um chupacabra (cc, inimigo) e uma flag
- * booleana indicando se o jogo está em "game over" ou não
- */
-// EXEMPLOS:
-const JOGO_INICIAL = {vaca: VACA_INICIAL, cc: CC_INICIAL, gameOver: false};
-const JOGO_INICIAL_PROX = {vaca: VACA_INICIAL_PROX, cc: CC_INICIAL_PROX, gameOver: false};
-const JOGO_INICIAL_CC_PEGA_VACA = {vaca: VACA_MEIO, cc: CC_MEIO, gameOver: false};
-const JOGO_INICIAL_CC_PEGA_VACA_PROX = {...JOGO_INICIAL_CC_PEGA_VACA, gameOver: true};
 
 
-/// -------------- FUNCOES
+/// FUNCOES
 
 /**
- * moveVaca: Vaca -> Vaca
+ * Vaca -> Vaca
  * Move a vaca nos eixos dx e dy
  * @param vaca: Vaca 
  * @returns Vaca
@@ -243,52 +183,12 @@ testes(() => {
 })
 
 
-
-/**
- * moveJogo: Jogo -> Jogo
- * Realiza as ações necessárias no jogo a cada tick
- * TODO
- */
-// stub:
-function moveJogo(jogo: Jogo): Jogo {
-    return jogo;
-}
-testes(() => {
-    describe("testes do moveJogo", () => {
-        test("transição de jogo sem colisões", () => {
-            expect(moveJogo(JOGO_INICIAL)).toStrictEqual(JOGO_INICIAL_PROX)
-        })
-    });
-})
-
-
-/**
- * desenhaJogo: Jogo -> Imagem
- * Desenha jogo
- * TODO
- */
-// stub:
-function desenhaJogo(jogo: Jogo): Imagem {
-    return TELA;
-}
-
-/**
- * trataTeclaJogo: Jogo, String -> Jogo
- * Trata eventos do teclado
- * TODO
- */
-// stub:
-function trataTeclaJogo(jogo: Jogo, tecla: string) {
-    return jogo;
-}
-
-
 function main() {
-    reactor(JOGO_INICIAL,   // Jogo
+    reactor(VACA_INICIAL,
         {
-            aCadaTick: moveJogo,   // Jogo -> Jogo
-            desenhar: desenhaJogo,  // Jogo -> Imagem
-            quandoTecla: trataTeclaJogo,  // Jogo, String -> Jogo
+            aCadaTick: moveVaca,
+            desenhar: desenhaVaca,
+            quandoTecla: trataTeclaVaca,
         })
 }
 
